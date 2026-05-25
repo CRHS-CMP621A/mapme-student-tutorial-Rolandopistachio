47de1bf422d2e612991edc6e12e8de073db4cee1
Rolandopistachio <Roland@solarsites.com>
2026-05-25 10:09:58 -0300

    made it to task 5.4 after some heavy difficulty

--- GeoLocationAPI/script.js
+++ GeoLocationAPI/script.js
@@ -1,13 +1,26 @@
-navigator.geolocation.getCurrentPosition( 
-        function (position){
-            // console.log(position)
-            const latitude = position.coords.latitude;
-            const longitude = position.coords.longitude;
-            console.log("https://www.google.com/maps/place/Halifax+Shopping+Centre/@44.6509699,-63.6233414,15.25z/data=!3m1!5s0x4b5a2199a65dc525:0xae9b62cd459a4a70!4m15!1m8!3m7!1s0x4b5a211407dbfac1:0x666be3a6438b2ddc!2sHalifax,+NS!3b1!8m2!3d44.6508608!4d-63.5923256!16s%2Fm%2F02qjb7z!3m5!1s0x4b5a21999741f353:0x93e0f53cd371939c!8m2!3d44.649249!4d-63.618682!16s%2Fm%2F02vpj73?entry=ttu&g_ep=EgoyMDI2MDUxMy4wIKXMDSoASAFQAw%3D%3D")
-            console.log(@44.6509699, -63.6233414);
-    },
-        function () {
-            alert("could not get position")
-        }
-);
+navigator.geolocation.getCurrentPosition(
+  function (position) {
+    // console.log(position)
+    const latitude = position.coords.latitude;
+    const longitude = position.coords.longitude;
+    const coords = [latitude, longitude];
+    var map = L.map("map").setView(coords, 13);
 
+    L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
+      attribution:
+        '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+    }).addTo(map);
+
+    L.marker([latitude, longitude])
+      .addTo(map)
+      .bindPopup("A pretty CSS popup.<br> Easily customizable.")
+      .openPopup();
+    console.log(
+      "https://www.google.com/maps/place/Halifax+Shopping+Centre/@44.6509699,-63.6233414,15.25z/data=!3m1!5s0x4b5a2199a65dc525:0xae9b62cd459a4a70!4m15!1m8!3m7!1s0x4b5a211407dbfac1:0x666be3a6438b2ddc!2sHalifax,+NS!3b1!8m2!3d44.6508608!4d-63.5923256!16s%2Fm%2F02qjb7z!3m5!1s0x4b5a21999741f353:0x93e0f53cd371939c!8m2!3d44.649249!4d-63.618682!16s%2Fm%2F02vpj73?entry=ttu&g_ep=EgoyMDI2MDUxMy4wIKXMDSoASAFQAw%3D%3D"
+    );
+    console.log(latitude, longitude);
+  },
+  function () {
+    alert("could not get position");
+  }
+);
--- MapApp/script.js
+++ MapApp/script.js
@@ -1,14 +1,175 @@
-'use strict';
+"use strict";
 
 // prettier-ignore
+
 const months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
 
-const form = document.querySelector('.form');
-const containerWorkouts = document.querySelector('.workouts');
-const inputType = document.querySelector('.form__input--type');
-const inputDistance = document.querySelector('.form__input--distance');
-const inputDuration = document.querySelector('.form__input--duration');
-const inputCadence = document.querySelector('.form__input--cadence');
-const inputElevation = document.querySelector('.form__input--elevation');
+const form = document.querySelector(".form");
+const containerWorkouts = document.querySelector(".workouts");
+const inputType = document.querySelector(".form__input--type");
+const inputDistance = document.querySelector(".form__input--distance");
+const inputDuration = document.querySelector(".form__input--duration");
+const inputCadence = document.querySelector(".form__input--cadence");
+const inputElevation = document.querySelector(".form__input--elevation");
+let workouts = [];
+let map;
+let mapEvent;
 
+///classes///
+class Workout {
+  date = new Date();
+  id = (Date.now() + "").slice(-10);
 
+  constructor(coords, distance, duration) {
+    this.coords = coords; //[lat,lng]
+    this.distance = distance; //in kms
+    this.duration = duration; //in mins
+  }
+}
+
+class Running extends Workout {
+  type = "Running";
+
+  constructor(coords, distance, duration, cadence) {
+    super(coords, distance, duration);
+    this.cadence = cadence;
+    this.calcpace();
+    this.setDescription();
+  }
+
+  //methods
+
+  calcpace() {
+    //min /km
+    this.pace = this.duration / this.distance;
+    return this.pace;
+  }
+
+  setDescription() {
+    this.description = `${this.date.toDateString()}`;
+  }
+}
+
+class Cycling extends Workout {
+  constructor(coords, distance, duration, elevationGain) {
+    super(coords, distance, duration);
+    this.elevation = elevationGain;
+  }
+}
+const Run1 = new Running([39, -12], 5.2, 24, 178);
+const Cycling1 = new Cycling([39, -12], 12, 95, 523);
+console.log(Run1, Cycling1);
+navigator.geolocation.getCurrentPosition(function (position) {
+  // console.log(position)
+  const latitude = position.coords.latitude;
+  const longitude = position.coords.longitude;
+
+  const coords = [latitude, longitude];
+  map = L.map("map").setView(coords, 13);
+
+  L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
+    attribution:
+      '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+  }).addTo(map);
+
+  map.on(
+    "click",
+    function (mapE) {
+      mapEvent = mapE;
+
+      form.classList.remove("hidden");
+      inputDistance.focus();
+    },
+    function () {
+      alert("could not get position");
+    }
+  );
+});
+form.addEventListener("submit", function (e) {
+  e.preventDefault();
+
+  const type = inputType.value;
+  const distance = Number(inputDistance.value);
+  const duration = Number(inputDuration.value);
+  const lat = mapEvent.latlng.lat;
+  const lng = mapEvent.latlng.lng;
+  let workout;
+  let html;
+  if (type === "running") {
+    const cadence = Number(inputCadence.value);
+    workout = new Running([lat, lng], distance, duration, cadence);
+    console.log(workout);
+    let html = `<li class="workout workout--running" data-id=${workout.id}>
+    <h2 class="workout__title">${workout.description}</h2>
+    <div class="workout__details">
+      <span class="workout__icon">🏃‍♂️</span>
+      <span class="workout__value">${workout.distance}</span>
+      <span class="workout__unit">km</span>
+    </div>
+    <div class="workout__details">
+      <span class="workout__icon">⏱</span>
+      <span class="workout__value">${workout.duration}</span>
+      <span class="workout__unit">min</span>
+    </div>
+    <div class="workout__details">
+      <span class="workout__icon">⚡️</span>
+      <span class="workout__value">${workout.pace}</span>
+      <span class="workout__unit">min/km</span>
+    </div>
+    <div class="workout__details">
+      <span class="workout__icon">🦶🏼</span>
+      <span class="workout__value">${workout.cadence}</span>
+      <span class="workout__unit">spm</span>
+    </div>
+  </li>`;
+    console.log(html);
+    form.insertAdjacentHTML("afterend", html);
+  }
+  workouts.push(workout);
+  console.log(workouts);
+
+  html += `<li class="workout workout--cycling" data-id="1234567891">
+<h2 class="workout__title">Cycling on April 5</h2>
+<div class="workout__details">
+  <span class="workout__icon">🚴‍♀️</span>
+  <span class="workout__value">27</span>
+  <span class="workout__unit">km</span>
+</div>
+<div class="workout__details">
+  <span class="workout__icon">⏱</span>
+  <span class="workout__value">95</span>
+  <span class="workout__unit">min</span>
+</div>
+<div class="workout__details">
+  <span class="workout__icon">⚡️</span>
+  <span class="workout__value">16</span>
+  <span class="workout__unit">km/h</span>
+</div>
+<div class="workout__details">
+  <span class="workout__icon">⛰</span>
+  <span class="workout__value">223</span>
+  <span class="workout__unit">m</span>
+</div>
+</li>`;
+
+  L.marker([lat, lng])
+    .addTo(map)
+    .bindPopup(
+      L.popup({
+        maxWidth: 250,
+        minWidth: 100,
+        autoClose: false,
+        closeOnClick: false,
+        className: "running-popup",
+      })
+    )
+    .setPopupContent("Workout")
+    .openPopup();
+
+  // code for adding map marker...
+});
+
+inputType.addEventListener("change", function () {
+  inputCadence.closest(".form__row").classList.toggle("form__row--hidden");
+  inputElevation.closest(".form__row").classList.toggle("form__row--hidden");
+});
